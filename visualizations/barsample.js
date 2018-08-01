0895e8c95d265173b643942c78ac15e0b5c141d2
yimej <33288736+yimej@users.noreply.github.com>
2018-08-01 18:49:42 -0400

    sample graphs. add possible dropdown menu.

---
+++ visualizations/barsample.js
@@ -0,0 +1,34 @@
+makePlot();
+
+function makePlot() {
+  d3.csv('/visualizations/sample.csv', function(data){makeBar(data)}); //enter file name
+};
+
+function makeBar(dataset) {
+  var x = [];
+  var y = [];
+
+  for (var i = 0; i < dataset.length - 1; i++) {
+    row = dataset[i];
+    x.push(row['x column']); //enter name of X column values
+    y.push(row['y column']); //enter name of Y column values
+  }
+
+  var trace = {
+    x: x, 
+    y: y,
+    type: "bar",
+    marker: {color: rgb(26, 82, 118)} //change to matching color
+  };
+
+  var data = [trace];
+  
+  var layout = {
+    title: "sample bar title", //enter title
+    xaxis: {title: "x label", tickangle: -35}, //enter x axis label
+    yaxis: {title: "y label"}, //enter y axis label
+    font: {size: 10}
+  };
+
+  Plotly.newPlot('bar', data, layout);
+};
---
+++ visualizations/linesample.js
@@ -0,0 +1,34 @@
+makePlot();
+
+function makePlot() {
+  d3.csv('sample.csv', function(data){makeLine(data)}); //enter file name
+};
+
+function makeLine(dataset) {
+  var x = [];
+  var y = [];
+
+  for (var i = 0; i < dataset.length - 1; i++) {
+    row = dataset[i];
+    x.push(row['x column']); //enter name of X column values
+    y.push(row['y column']); //enter name of Y column values
+  }
+
+  var trace = {
+    x: x, 
+    y: y,
+    type: "line",
+    marker: {color: rgb(26, 82, 118)} //change to matching color
+  };
+
+  var data = [trace];
+
+  var layout = {
+    title: "sample line title", //enter title
+    xaxis: {title: "x label", tickangle: -35}, //enter x axis label
+    yaxis: {title: "y label"}, //enter y axis label
+    font: {size: 10}
+  };
+
+  Plotly.newPlot('line', data, layout);
+};
