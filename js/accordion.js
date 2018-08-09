f51a3b940eeca7ef5774f0209b43ec7db6b55a97
yimej <33288736+yimej@users.noreply.github.com>
2018-08-09 14:01:49 -0400

    add accordion table

--- js/accordion.js
+++ js/accordion.js
@@ -1,3 +1,12 @@
+function accordion(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
 //EXPLORE
 var slideIndex = 1;
 showDivs(slideIndex);
