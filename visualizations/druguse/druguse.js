9c97000e65363a19820154673ee6b4d37e6768b6
yimej <33288736+yimej@users.noreply.github.com>
2018-08-09 12:50:50 -0400

    edit druguse.js to reflect %

--- visualizations/druguse/druguse.js
+++ visualizations/druguse/druguse.js
@@ -6,65 +6,38 @@ function makeDrugUsePlot() {
 
 function makeDrugUse(dataset) {
   var x = [];
-  var y1 = [];
-  var y2 = [];
+  var yes = [];
+  var no = [];
 
-  for (var i = 0; i < dataset.length - 1; i++) {
-    row = dataset[i];
-    x.push(row['x column']); //enter name of X column values
-    y1.push(row['Yes']); //enter name of Y column values
-    y2.push(row['No'])
-  }
+  // for (var i = 0; i < dataset.length - 1; i++) {
+  //   row = dataset[i];
+  //   // x.push(row['x column']); //enter name of X column values
+  //   yes.push(row['Yes']); //enter name of Y column values
+  //   no.push(row['No'])
+  // }
 
   var trace1 = {
-    x:x,
-    y:y1,
+    x: ['Never Married, Not Cohabitating', 'Never Married, Cohabitating', 'Married', 'Legally Separated', 'Divorced', 'Widowed'],
+    y: [23.04568528, 20.77431539, 12.52452583, 18.01801802, 16.36363636, 6.25],    
     name:'Yes',
-    type:'bar'
+    type:'bar',
+    marker:{color:'darkred'}
   };
 
   var trace2 = {
-    x:x,
-    y:y2,
+    x: ['Never Married, Not Cohabitating', 'Never Married, Cohabitating', 'Married', 'Legally Separated', 'Divorced', 'Widowed'],
+    y: [76.95431472, 79.22568461, 87.47547417, 81.98198198, 83.63636364, 93.75],
     name:'No',
     type:'bar'
   };
 
-  // var trace3 = {
-  //   x:['Yes', 'No'],
-  //   y:[12.52452583, 87.47547417],
-  //   name:'Married',
-  //   type:'bar'
-  // };
-
-  // var trace4 = {
-  //   x:['Yes', 'No'],
-  //   y:[18.01801802, 81.98198198],
-  //   name:'Separated',
-  //   type:'bar'
-  // };
-
-  // var trace5 = {
-  //   x:['Yes', 'No'],
-  //   y:[16.36363636, 83.63636364],
-  //   name:'Divorced',
-  //   type:'bar'
-  // };
-
-  // var trace6 = {
-  //   x:['Yes', 'No'],
-  //   y:[6.25, 93.75],
-  //   name:'Widowed',
-  //   type:'bar'
-  // };
-
-  var data = [trace1, trace2];//trace6, trace5, trace4, trace3, trace2, trace1];
+  var data = [trace2, trace1];
   
   var layout = {
     title: "Marijuana Use vs. Marital Status", //enter title
-    xaxis: {title: "Marijuana Use in Last 12 Months (Age 34)"}, //enter x axis label
+    xaxis: {title: "Marijuana Use in Last 12 Months (Age 34)", tickangle: -25}, //enter x axis label
     yaxis: {title: "% of Responses with Reported Marital Status"}, //enter y axis label
-    font: {size: 10},
+    font: {size: 9},
     barmode: 'stack'
   };
 
