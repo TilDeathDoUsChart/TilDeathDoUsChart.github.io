23bff9d68005d3835ea3f1f06e6b9a07014a6c4d
yimej <33288736+yimej@users.noreply.github.com>
2018-08-10 11:23:56 -0400

    top3 tick text

---
+++ visualizations/druguse/.ipynb_checkpoints/druguse-checkpoint.js
@@ -0,0 +1,45 @@
+makeDrugUsePlot();
+
+function makeDrugUsePlot() {
+  d3.csv('visualizations/druguse/druguse.csv', function(data){makeDrugUse(data)}); //enter file name
+};
+
+function makeDrugUse(dataset) {
+  var x = [];
+  var yes = [];
+  var no = [];
+
+  // for (var i = 0; i < dataset.length - 1; i++) {
+  //   row = dataset[i];
+  //   // x.push(row['x column']); //enter name of X column values
+  //   yes.push(row['Yes']); //enter name of Y column values
+  //   no.push(row['No'])
+  // }
+
+  var trace1 = {
+    x: ['Never Married, Not Cohabitating', 'Never Married, Cohabitating', 'Married', 'Legally Separated', 'Divorced', 'Widowed'],
+    y: [23.04568528, 20.77431539, 12.52452583, 18.01801802, 16.36363636, 6.25],    
+    name:'Yes',
+    type:'bar',
+    marker:{color:'darkred'}
+  };
+
+  var trace2 = {
+    x: ['Never Married, Not Cohabitating', 'Never Married, Cohabitating', 'Married', 'Legally Separated', 'Divorced', 'Widowed'],
+    y: [76.95431472, 79.22568461, 87.47547417, 81.98198198, 83.63636364, 93.75],
+    name:'No',
+    type:'bar'
+  };
+
+  var data = [trace2, trace1];
+  
+  var layout = {
+    title: "Marijuana Use vs. Marital Status at Age 34", //enter title
+    xaxis: {title: "Marijuana Use in Last 12 Months", tickangle: -25}, //enter x axis label
+    yaxis: {title: "% of Responses with Reported Marital Status"}, //enter y axis label
+    font: {size: 9},
+    barmode: 'stack'
+  };
+
+  Plotly.newPlot('druguse', data, layout);
+};
--- visualizations/top3/.ipynb_checkpoints/top3_2-checkpoint.js
+++ visualizations/top3/.ipynb_checkpoints/top3_2-checkpoint.js
@@ -26,12 +26,10 @@ function makeTop3_2Scatter(dataset) {
   
   var layout = {
     title: "Marital Status in July 2014 (Age 23) vs. LCI", //enter title
-    xaxis: {
-      title: "Marital Status in July 2014 (Age 23)", 
-      tickangle: -25, //enter x axis label
-      ticktext: ['Never Married, Not Cohabitating', 'Never Married, Cohabitating', 'something random', 'Married', 'Legally Separated', 'Divorced', 'Widowed']},
+    xaxis: {title: "Marital Status in July 2014 (Age 23)", tickangle: -25}, //enter x axis label
     yaxis: {title: "LCI"}, //enter y axis label
-    font: {size: 9}
+    font: {size: 9},
+    ticktext: ['Never Married, Not Cohabitating', 'Never Married, Cohabitating', 'Married', 'Legally Separated', 'Divorced', 'Widowed']
   };
 
   Plotly.newPlot('top3_2', data, layout)
